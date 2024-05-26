32e49f52e16d329eb90c3dd677212e669a99a09b
iyyafi <iyyafi@gmail.com>
2024-05-26 17:53:17 +0700

    add adapter for ssr

--- frontend-astro/astro.config.mjs
+++ frontend-astro/astro.config.mjs
@@ -2,16 +2,18 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   experimental: {
-    actions: true,
+    actions: true
   },
-  integrations: [
-    solidJs(),
-    tailwind({
-      nesting: true,
-    }),
-  ],
+  integrations: [solidJs(), tailwind({
+    nesting: true
+  })],
+  adapter: node({
+    mode: "standalone"
+  })
 });
